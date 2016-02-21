d68c1790209ed2c172d6e3c928f1536eae3a07df
rwatalin <rwatalin@mix.wvu.edu>
2016-02-21 16:00:07 -0500

    Completed Welp Assignment RW

--- Welp/Welp/main.m
+++ Welp/Welp/main.m
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[]) {
         Restaurant *chipoltle = [[Restaurant alloc]initWithName:@"Chipoltle" phone:@"(304)555-5555" address:@"Right by the new Starbucks" andHours:chipoltleHours];
         
         NSDictionary *chipoltleDict = [chipoltle toDictionary];
-        
+        NSLog(@"%@", chipoltleDict);
     }
     return 0;
 }
